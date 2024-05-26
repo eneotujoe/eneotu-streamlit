aa8f9c8930df9e55f2e5f15d08c5afac859fb348
eneotujoe <eneotujoe@gmail.com>
2024-05-26 22:53:33 +0100

    fixing plotly issue

--- pages/2_Dashboard.py
+++ pages/2_Dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.graph_objects as go
 import plotly.express as px
+import plotly.graph_objects as go
 import numpy as np
 
 st.set_page_config(
